baf642429355030f2b6e86445ac41c21756f07b7
Ivan Meshcheryakov <meshcheryakovvrn@gmail.com>
2023-02-09 01:14:00 +0400

    Module #4.2. Final Task

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,27 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        int half = cathetusLength - 1;
 
+        for (int row = 1; row <= cathetusLength; row++) {
+            int colLength = half + 1;
+            for (int col = 1, num = cathetusLength; col <= colLength; col++) {
+                if (num > row) {
+                    System.out.print(' ');
+                } else if (num <= row) {
+                    System.out.print('*');
+                }
+
+                if (col == colLength) {
+                    System.out.println();
+                }
+                if (col <= half) {
+                    num--;
+                } else {
+                    num++;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,27 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int half = cathetusLength - 1;
 
+        for (int row = 1; row <= cathetusLength; row++) {
+            int colLength = half + row;
+            for (int col = 1, num = cathetusLength; col <= colLength; col++) {
+                if (num > row) {
+                    System.out.print(' ');
+                } else if (num <= row) {
+                    System.out.print(num);
+                }
+
+                if (col == colLength) {
+                    System.out.println();
+                }
+                if (col <= half) {
+                    num--;
+                } else {
+                    num++;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
